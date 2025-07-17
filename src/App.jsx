92d65686c110d88cb458e3ed1a6fccfdd47d658c
Saad Bin Khalid <saadbinkhalid1895@gmail.com>
2025-07-17 12:35:20 +0500

    Initialized Repo with some code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      {/* <About/>
+      <Services/>
+      <Testimonials/>
+      <Pricing/>
+      <Contact/>
+      <Footer/> */}
+    </>
+  );
+}
+
+export default App;
