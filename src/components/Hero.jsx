d9d2a312548274fa4436a439def182c8aed736d1
Saad Bin Khalid <saadbinkhalid1895@gmail.com>
2025-07-19 20:23:29 +0500

    Fixed some issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const imageVariants = {
 
 const Hero = () => {
   return (
-    <section className="min-h-screen bg-background text-white flex flex-col-reverse md:flex-row items-center md:items-center justify-center gap-10 px-6 md:px-12 pb-12 md:pb-24 pt-32">
+    <section className="min-h-screen bg-background text-white flex flex-col-reverse md:flex-row items-center md:items-center justify-center gap-10 pb-12 md:pb-24 pt-32">
       {/* Text Section */}
       <motion.div
         className="w-full md:w-1/2 flex flex-col gap-4 text-center md:text-left p-4"
@@ -56,13 +56,9 @@ const Hero = () => {
 
         <motion.a
           href="#contact"
-          className="mt-4 inline-block bg-primary text-white font-semibold px-6 py-3 rounded-md w-max mx-auto md:mx-0"
+          className="mt-4 inline-block bg-primary text-white text-xl font-semibold px-6 py-3 rounded-md w-max mx-auto md:mx-0 hover:text-[#108b7a] duration-300" 
           variants={textVariants}
           custom={3}
-          whileHover={{
-            scale: 1.05,
-            boxShadow: "0px 0px 12px rgba(59, 130, 246, 0.6)",
-          }}
           whileTap={{ scale: 0.95 }}
         >
           Get Qualified Leads Now
